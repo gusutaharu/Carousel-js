ab6050daedb14a886b48be8a76775b510de380a6
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-10-05 09:17:30 +0900

    lesson09

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 'use strict'
 {
-  
+  const next  =  document.getElementById('next');
+  const ul  = document.querySelector('ul');
+
+  next.addEventListener('click', ()=>{
+    ul.style.transform = 'translateX(-200px)';
+  })
 }
