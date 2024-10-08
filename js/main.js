239c7523ff088f3f3e219e653736e1fba3850204
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-10-08 10:11:47 +0900

    lesson18

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
   const prev  =  document.getElementById('prev');
   const ul  = document.querySelector('ul');
   const slides  = ul.children;
+  const dots = []
   let currentIndex  = 0;
 
   function updateButtons() {
@@ -23,7 +24,16 @@
     ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
   }
 
+  function setupDots() {
+    for (let i = 0; i < slides.length; i++) {
+      const button = document.createElement('button');
+      dots.push(button);
+      document.querySelector('nav').appendChild(button);
+    }
+  }
+
   updateButtons();
+  setupDots();
 
   next.addEventListener('click', ()=>{
     currentIndex++;
